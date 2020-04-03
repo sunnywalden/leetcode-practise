ff0fb88f659e103012e2ddc57a5d653a9b73d110
zhangbo <zhangbo@tezign.com>
2020-04-03 17:42:03 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python3 leetcode practise
+
---
+++ num_sum_check.py
@@ -0,0 +1,26 @@
+#!env/bin/python
+# -*- coding: utf-8 -*-
+
+
+def get_nums(num=0, num_list=[]):
+    """
+    给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+    :param num: int
+    :param num_list: list
+    :return:
+    """
+    filtered_nums = list(filter(lambda i: i <= num, num_list))
+    filtered_nums.sort()
+    for index, number in enumerate(filtered_nums):
+        if num - number in filtered_nums[index:]:
+            m = number
+            n = num - number
+            return num_list.index(m), num_list.index(n)
+    return None, None
+
+
+if __name__ == '__main__':
+    m, n = get_nums(9, [2, 7, 11, 15])
+    print(m, n)
