3a581d3c9601ba2cb5bedb8dc1aa9de950c22f3a
zhangbo <zhangbo@tezign.com>
2020-04-09 19:20:43 +0800

    string to int optimize

--- 08_string_to_int.py
+++ 08_string_to_int.py
@@ -10,13 +10,13 @@ def string_to_int(s):
     int_string = []
 
     if not source_str: return 0
-    if source_str[0] not in ['-', '+'] + num_string:
+    if source_str[0] not in ['-', '+'] and not source_str.isdigit():
         return 0
     else:
         int_string.append(source_str[0])
 
     for c in source_str[1:]:
-        if c not in num_string:
+        if not c.isdigit():
             break
         else:
             int_string.append(c)
@@ -30,13 +30,16 @@ def string_to_int(s):
         return 2 ** 31 -1
 
 
+
 if __name__ == '__main__':
     s1 = ' -fer4ce3'
     s2 = '4gth6hj'
     s3 = '-3467843654765787dtrgt'
     s4 = '53575862432453647bgfnb'
     res1 = string_to_int(s1)
+
     res2 = string_to_int(s2)
+
     res3 = string_to_int(s3)
     res4 = string_to_int(s4)
     print(res1, res2, res3, res4)
