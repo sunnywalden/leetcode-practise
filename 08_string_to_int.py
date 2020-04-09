f66266ad20c3c8e3f7dbd11b8769f07d9131b392
zhangbo <zhangbo@tezign.com>
2020-04-09 19:14:15 +0800

    string to int optimize

--- 08_string_to_int.py
+++ 08_string_to_int.py
@@ -8,31 +8,37 @@ def string_to_int(s):
     source_str = s.strip()
 
     int_string = []
-    num_string = [str(i) for i in range(10)]
 
-    if not source_str: return None
+    if not source_str: return 0
     if source_str[0] not in ['-', '+'] + num_string:
-        return None
+        return 0
     else:
         int_string.append(source_str[0])
 
-    int_string += list(filter(lambda c: c in num_string, source_str[1:]))
+    for c in source_str[1:]:
+        if c not in num_string:
+            break
+        else:
+            int_string.append(c)
 
-    num = int(''.join(int_string))
-
-    if num >= -231 and num <= 230:
+    num = int(''.join(int_string)) if int_string[0].isdigit() and len(int_string) >= 2 or len(int_string)>=1 and  int_string[0] not in ['-', '+'] else 0
+    if num >= -2**31 and num <= 2 ** 31 -1:
         return num
-    elif num < -231:
-        return -231
+    elif num < -2**31:
+        return -2**31
     else:
-        return 230
+        return 2 ** 31 -1
 
 
 if __name__ == '__main__':
     s1 = ' -fer4ce3'
     s2 = '4gth6hj'
+    s3 = '-3467843654765787dtrgt'
+    s4 = '53575862432453647bgfnb'
     res1 = string_to_int(s1)
     res2 = string_to_int(s2)
-    print(res1, res2)
+    res3 = string_to_int(s3)
+    res4 = string_to_int(s4)
+    print(res1, res2, res3, res4)
 
 
